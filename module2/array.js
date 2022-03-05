246e6ebf0d34cb01a0e07c5d71c76d62d85a16da
mohd-aman <saifiamaan445@gmail.com>
2022-03-05 23:31:19 +0530

    methods of array added

--- module2/array.js
+++ module2/array.js
@@ -22,3 +22,27 @@ console.log("###############################################")
 console.log("Array before push: ",eleArr);
 eleArr.push("new item");
 console.log("Array after push : ",eleArr);
+
+//2. pop
+
+console.log("Array before pop : ",eleArr);
+eleArr.pop();
+console.log("Array after pop: ",eleArr);
+
+//3. shift
+
+console.log("Array before shift: ",eleArr);
+eleArr.shift();
+console.log("Array after shift: ",eleArr);
+
+
+//4. unshift
+
+console.log("Array before unshift: ",eleArr);
+eleArr.unshift("newly added item");
+console.log("Array after unshift: ",eleArr);
+
+//length
+
+let len = eleArr.length;
+console.log(len);
