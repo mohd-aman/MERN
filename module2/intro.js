60e6f7528843c6c15f9c609ce0227ff45694bb2f
mohd-aman <saifiamaan445@gmail.com>
2022-03-05 21:55:45 +0530

    intro to js

---
+++ module2/intro.js
@@ -0,0 +1,37 @@
+// printing in javaScript
+console.log("Swagat hai aap sabhi logo ka!!!!!");
+
+console.log("Pepcoders");
+
+
+// variables in js and data types
+
+let num = 10; // Number
+console.log(num);
+
+let char = 'a';// character
+console.log(char);
+
+let str = 'I am string'; //string
+console.log(str);
+
+let bool = true; // boolean
+console.log(bool);
+
+//loops 
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+
+for(let i=1;i<5;i++){
+    console.log(i);
+}
+
+let count = 10;
+while(count>0){
+    console.log(count);
+    count--;
+}
+
