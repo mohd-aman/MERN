c31927d2effc9eba5cc43060fc78fedc55ec7d64
mohd-aman <saifiamaan445@gmail.com>
2022-03-08 22:16:43 +0530

    object in js

---
+++ module2/object.js
@@ -0,0 +1,27 @@
+//empty object
+// let obj = {};
+// console.log(obj);
+
+// let objAman = {
+//     Name: "Mohd Aman",
+//     Age: 45,
+//     phoneNo: 8860503744,
+//     lastName:'Aman'
+// };
+
+// console.log(objAman);
+
+let capAmerica = {
+    Name: 'Steve',
+    Age: 9999,
+    Friends: ['Natasha','Thor','Tony','Bucky','Bruce'],
+    address: {
+        city:"Queens",
+        state:'Haryana'
+    },
+    sayHi:function(){
+        console.log("Cap America says Hiiiii")
+    }
+}
+
+console.log(capAmerica);
