86244a84091465a93836499657e84fed309e830c
mohd-aman <saifiamaan445@gmail.com>
2022-03-10 21:48:03 +0530

    child_process

--- module2/introTonode/childProcess.js
+++ module2/introTonode/childProcess.js
@@ -4,5 +4,15 @@ let cp =  require('child_process');
 //open calculator with the help of this file
 
 // for windows replace gnome-calculator with calc
-cp.execFileSync("gnome-calculator");
+//for mac replace gnome-calculator with open -a Calculator
+
+// cp.execSync("gnome-calculator");
+//open vscode with childProcess
+
+// cp.execSync("code");
+
+let content = cp.execSync("node test.js");
+
+console.log(""+content);
+
 
