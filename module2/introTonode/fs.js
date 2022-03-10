937bda74a47c7876eec93655193da509f218c955
mohd-aman <saifiamaan445@gmail.com>
2022-03-10 23:31:17 +0530

    CRUD for file completed

--- module2/introTonode/fs.js
+++ module2/introTonode/fs.js
@@ -5,5 +5,19 @@ let path = require("path");
 let filePath = path.join(__dirname,"file.txt");
 // console.log(filePath);
 
+//C-create
 //it creates file if it does not exists else it override
-fs.writeFileSync(filePath,"Again writing on existing file");
+fs.writeFileSync(filePath,"Hello I am a text file ");
+
+//R-read
+console.log("Before update : ")
+let content = fs.readFileSync(filePath,'utf-8');
+console.log(content);
+
+//U-update
+fs.appendFileSync(filePath,"\nNewly added content");
+console.log("After update : ")
+console.log(fs.readFileSync(filePath,'utf-8'));
+
+//D-delete
+fs.unlinkSync(filePath);
