0314cfa66d97087f35baeb7885eec1ca4aedad50
mohd-aman <saifiamaan445@gmail.com>
2022-03-10 23:53:20 +0530

    calculator module

---
+++ module2/introTonode/module/calculator.js
@@ -0,0 +1,29 @@
+function add(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+function div(a,b){
+    return a/b;
+}
+
+function mul(a,b){
+    return a*b;
+}
+
+module.exports = {
+    addition:add,
+    subtraction:sub,
+    division:div,
+    multiply:mul
+}
+
+
+
+
+
+
+
---
+++ module2/introTonode/module/testModule.js
@@ -0,0 +1,8 @@
+let calc = require("./calculator.js");
+// console.log(calc);
+
+console.log(calc.addition(4,6))
+
+console.log(calc.division(10,2))
+
+console.log(calc.multiply(2,5));
