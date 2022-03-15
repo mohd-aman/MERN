6a316c4431cb1ff88d270f885845580c5eab37b8
mohd-aman <saifiamaan445@gmail.com>
2022-03-15 23:36:58 +0530

    q5 done

--- module2/introTonode/questions/q5.js
+++ module2/introTonode/questions/q5.js
@@ -1,1 +1,12 @@
 //move a file
+
+let fs = require("fs");
+let path = require("path");
+
+let srcPath = path.join(__dirname,"file1.txt");
+// console.log(srcPath);
+let destPath = path.join(__dirname,"newFolder","file1.txt");
+// console.log(destPath);
+fs.copyFileSync(srcPath,destPath);
+
+fs.unlinkSync(srcPath);
--- module2/introTonode/questions/q6.js
+++ module2/introTonode/questions/q6.js
@@ -1,4 +1,4 @@
 //read content of unorganised folder and print each file type
 //for example abc.mp3 --> print Audio File
-//            xyz.mp4 --> print Vidoe File
+//            xyz.mp4 --> print Video File
 //            fsd.jpeg -> print Image File
