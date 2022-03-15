56a174fa43cb65cdf6b911271fa753e8d1373fa9
mohd-aman <saifiamaan445@gmail.com>
2022-03-15 22:55:00 +0530

    q3 solved

--- module2/introTonode/questions/q3.js
+++ module2/introTonode/questions/q3.js
@@ -1,1 +1,11 @@
-//copy index.html file from module1 to a new folder inside module2 having name html.
+//Q. copy index.html file from module1 to a new folder inside module2 having name html.
+
+let fs = require("fs");
+let path = require("path");
+
+let srcPath = path.join(__dirname,"..","..","..","module1","index.html");
+console.log("source path --> ",srcPath);
+let destPath = path.join(__dirname,"..","..","index.html");
+console.log("destination --> ",destPath);
+
+fs.copyFileSync(srcPath,destPath);
