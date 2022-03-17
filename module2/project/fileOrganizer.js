f8982aacfec213c4d295d332454961e5549fbc04
mohd-aman <saifiamaan445@gmail.com>
2022-03-17 21:34:00 +0530

    input

---
+++ module2/project/fileOrganizer.js
@@ -0,0 +1,6 @@
+// console.log("Hello world");
+let inputArr = process.argv;
+console.log(inputArr);
+// ['pathToNode','pathToFile','input_passed_in_terminal']
+let input = inputArr[2];
+console.log(input);
