4c04c38513dd2aabe6412a0fce9904ff038f2020
mohd-aman <saifiamaan445@gmail.com>
2022-03-17 23:17:30 +0530

    project completed

--- module2/project/fileOrganizer.js
+++ module2/project/fileOrganizer.js
@@ -5,7 +5,6 @@ let folderPath = process.argv[2];
 // console.log(folderPath);
 let folderExists = fs.existsSync(folderPath);
 
-
 let extensions = {
     Audio:[".mp3"],
     Video:[".mp4",".mkv"],
@@ -14,7 +13,6 @@ let extensions = {
     Software:[".exe"]
 };
 
-
 if(folderExists){
     //we will code
     // console.log("path is valid!!!!!!");
@@ -22,9 +20,16 @@ if(folderExists){
     for(let i=0;i<files.length;i++){
         let ext = path.extname(files[i]);
         let nameOfFolder = giveFolderName(ext);
-        console.log("Ext--",ext,"Folder--",nameOfFolder);
+        // console.log("Ext--",ext,"Folder--",nameOfFolder);
+        let pathOfFolder = path.join(folderPath,nameOfFolder); 
+        let exist = fs.existsSync(pathOfFolder);
+        if(exist){
+            moveFile(folderPath,pathOfFolder,files[i]);
+        }else{
+            fs.mkdirSync(pathOfFolder);
+            moveFile(folderPath,pathOfFolder,files[i]);
+        }
     }
-    
 }
 else{
     console.log("Please Enter a Valid Path!!!!!!!");
@@ -41,4 +46,12 @@ function giveFolderName(ext){
         }
     }
     return 'Others'
+}
+
+
+function moveFile(folderPath,pathOfFolder,fileName){
+    let sourcePath = path.join(folderPath,fileName);
+    let destinationPath = path.join(pathOfFolder,fileName);
+    fs.copyFileSync(sourcePath,destinationPath);
+    fs.unlinkSync(sourcePath);
 }
