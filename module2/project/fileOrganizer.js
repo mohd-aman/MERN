39e2fb5a64f12f5ca46950bff8c67d1abcb77fe0
mohd-aman <saifiamaan445@gmail.com>
2022-03-17 22:35:12 +0530

    giveFolderName fxn implemented

--- module2/project/fileOrganizer.js
+++ module2/project/fileOrganizer.js
@@ -1,15 +1,44 @@
 let fs = require("fs");
+let path = require("path");
 let folderPath = process.argv[2];
 
 // console.log(folderPath);
 let folderExists = fs.existsSync(folderPath);
 
+
+let extensions = {
+    Audio:[".mp3"],
+    Video:[".mp4",".mkv"],
+    Document:[".doc",".xlsx",".pdf",".txt"],
+    Image:[".jpeg",".jpg",".png",".gif"],
+    Software:[".exe"]
+};
+
+
 if(folderExists){
     //we will code
     // console.log("path is valid!!!!!!");
     let files = fs.readdirSync(folderPath);
-    console.log(files);
+    for(let i=0;i<files.length;i++){
+        let ext = path.extname(files[i]);
+        let nameOfFolder = giveFolderName(ext);
+        console.log("Ext--",ext,"Folder--",nameOfFolder);
+    }
+    
 }
 else{
     console.log("Please Enter a Valid Path!!!!!!!");
+}
+
+
+function giveFolderName(ext){
+    for(let key in extensions){
+        let extArr = extensions[key];
+        for(let i=0;i<extArr.length;i++){
+            if(extArr[i] == ext){
+                return key;
+            }
+        }
+    }
+    return 'Others'
 }
