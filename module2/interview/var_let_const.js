a371f5a7b2aa39250ae60264d8394379dadab995
mohd-aman <saifiamaan445@gmail.com>
2022-03-24 23:02:17 +0530

    var let const

---
+++ module2/interview/var_let_const.js
@@ -0,0 +1,33 @@
+//hoisting
+// console.log(a);
+// var a = 10;
+
+// console.log(a);
+// var a;
+// a = 10;
+// console.log(a);
+
+// a = 20;
+
+// console.log(a);
+
+
+// var a = 50;
+// console.log(a);
+
+// console.log(a);
+// let a ;
+// a = 10;
+// console.log(a);
+
+// a = 20;
+// console.log(a);
+
+// let a = 40;
+// console.log(a);
+
+//const
+
+const a = 20;
+console.log(a);
+
