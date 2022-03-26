b9015699363ceea41f015260eec500f04604e54a
mohd-aman <saifiamaan445@gmail.com>
2022-03-26 23:19:55 +0530

    jsdom request activity

--- module2/web_scrapping/request.js
+++ module2/web_scrapping/request.js
@@ -1,19 +1,11 @@
 const request = require('request');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
 
-const link = "https://www.espncricinfo.com/series/ipl-2021-1249214/chennai-super-kings-vs-kolkata-knight-riders-final-1254117/full-scorecard";
+request('http://www.google.com', cb);
 
-request(link, cb);
-
-function cb(error, response, html) {
+function cb(error, response, body) {
     if(error)
         console.error('error:', error); // Print the error if one occurred
     else{
-        const dom = new JSDOM(html);
-        const document = dom.window.document;
-        let teamsName = document.querySelectorAll(".match-info.match-info-MATCH.match-info-MATCH-half-width .teams .name");
-        console.log(teamsName[0].textContent);
-        console.log(teamsName[1].textContent);
+        console.log('body:', body); // Print the HTML for the Google homepage.
     }
 }
