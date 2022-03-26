81d2099dff111819044e2265d15eb94d3850bbe2
mohd-aman <saifiamaan445@gmail.com>
2022-03-26 22:20:01 +0530

    callback

--- module2/web_scrapping/callback.js
+++ module2/web_scrapping/callback.js
@@ -1,1 +1,16 @@
 const fs = require("fs");
+
+console.log("Before ");
+
+fs.readFile("file.txt",cb);
+
+function cb(error,data){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(data+"");
+    }
+}
+
+
+console.log("After");
