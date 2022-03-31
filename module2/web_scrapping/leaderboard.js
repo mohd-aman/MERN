9e0f6a9e50d81ebd8cbcbcd1af43ba977b19404e
mohd-aman <saifiamaan445@gmail.com>
2022-03-31 23:40:09 +0530

    process player fxn added

--- module2/web_scrapping/leaderboard.js
+++ module2/web_scrapping/leaderboard.js
@@ -4,6 +4,8 @@ const { JSDOM } = jsdom;
 
 const link = "https://www.espncricinfo.com/series/ipl-2021-1249214/match-results";
 
+let leaderboard = [];
+
 request(link,cb);
 
 function cb(error,response,html){
@@ -41,4 +43,36 @@ function cb2(error,response,html){
             }
         }
     }
+}
+
+processPlayer('Rohit','15','4','2','4','0');
+processPlayer('Virat','50','20','4','3','3')
+processPlayer('Rohit','40','20','1','2','4');
+console.log(leaderboard);
+
+function processPlayer(name,runs,balls,fours,sixes){
+    runs = Number(runs);
+    balls = Number(balls);
+    fours = Number(fours);
+    sixes = Number(sixes);
+    for(let i=0;i<leaderboard.length;i++){
+        let playerObj = leaderboard[i];
+        if(playerObj.Name == name){
+            //will do some work here
+            playerObj.Runs+=runs;
+            playerObj.Balls+=balls;
+            playerObj.Fours+=fours;
+            playerObj.Sixes+=sixes;
+            return;
+        }
+    }
+    // code coming here means we did not find our player inside leaderboard
+    let obj = {
+        Name:name,
+        Runs:runs,
+        Balls:balls,
+        Fours:fours,
+        Sixes:sixes
+    }
+    leaderboard.push(obj);
 }
