bb450bbfefe499b187ab86b40e9af551a4beb7b3
mohd-aman <saifiamaan445@gmail.com>
2022-03-31 23:07:20 +0530

    leaderboard started

--- module2/web_scrapping/leaderboard.js
+++ module2/web_scrapping/leaderboard.js
@@ -16,7 +16,29 @@ function cb(error,response,html){
         for(let i=0;i<allScorecardTags.length;i++){
             let link = allScorecardTags[i].href;
             let completeLink = "https://www.espncricinfo.com"+link;
-            console.log(completeLink);
+            // console.log(completeLink);
+            request(completeLink,cb2);
+        }
+    }
+}
+
+function cb2(error,response,html){
+    if(error){
+        console.log(error);
+    }else{
+        const dom = new JSDOM(html);
+        const document = dom.window.document;
+        let batsmenRow = document.querySelectorAll(".table.batsman tbody tr");
+        for(let i=0;i<batsmenRow.length;i++){
+            let cells = batsmenRow[i].querySelectorAll("td");
+            if(cells.length == 8){
+                let name = cells[0].textContent;
+                let runs = cells[2].textContent;
+                let balls = cells[3].textContent;
+                let fours = cells[5].textContent;
+                let sixes = cells[6].textContent;
+                // console.log("Name : ",name,"Runs : ",runs,"Balls : ",balls,"Fours : ",fours,"Sixes : ",sixes);
+            }
         }
     }
 }
