24ac71d7452e33f141634ad487b841cb2f6a48ad
mohd-aman <saifiamaan445@gmail.com>
2022-04-14 22:59:12 +0530

    puppeteer browser opened

---
+++ module2/automation/raw.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer');
+
+// (async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto('https://example.com');
+//   await page.screenshot({ path: 'example.png' });
+
+//   await browser.close();
+// })();
+
+let browserPromise = puppeteer.launch({ headless: false });
+
+browserPromise.then(function(browserInstance){
+    console.log("Browser is opened");
+    let pagePromise = browserInstance.newPage();
+})
