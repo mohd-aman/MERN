e59719e80d01f0c79c380bd6f6030479a6b1fe7f
mohd-aman <saifiamaan445@gmail.com>
2022-04-14 23:36:27 +0530

    google opened

--- module2/automation/raw.js
+++ module2/automation/raw.js
@@ -1,16 +1,15 @@
 const puppeteer = require('puppeteer');
 
-// (async () => {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto('https://example.com');
-//   await page.screenshot({ path: 'example.png' });
-
-//   await browser.close();
-// })();
-
 let browserPromise = puppeteer.launch({ headless: false });
 
-browserPromise.then(function(browserInstance){
+browserPromise.then(function(browser){
     console.log("Browser is opened");
+    let pagePromise = browser.newPage();
+    return pagePromise;
+}).then(function(page){
+    console.log("page is opened");
+    let urlPromise = page.goto('https://www.google.com/');
+    return urlPromise
+}).then(function(){
+    console.log("google is opened");
 })
