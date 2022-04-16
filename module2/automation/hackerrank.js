4fc6729f572cfe7fd16e2682351d61d00fa50131
mohd-aman <saifiamaan445@gmail.com>
2022-04-16 23:24:02 +0530

    hk login successful

--- module2/automation/hackerrank.js
+++ module2/automation/hackerrank.js
@@ -44,4 +44,25 @@ browserPromise.then(function(browser){
     return clickPromse;
 }).then(function(){
     console.log("Login successful hogya hai ")
+    let waitPromise = page.waitForSelector('[data-automation="algorithms"]');
+    return waitPromise;
+}).then(function(){
+    let clickPromise = page.click('[data-automation="algorithms"]');
+    return clickPromise;
+}).then(function(){
+    return page.waitForSelector(".filter-group");
+}).then(function(){
+    let domSelectPromise = page.evaluate(function(){
+        let allDivs = document.querySelectorAll(".filter-group");
+        let div = allDivs[3];
+        let clickSelector = div.querySelector(".ui-checklist-list-item input");
+        clickSelector.click();
+        return;
+    })
+    return domSelectPromise;
+}).then(function(){
+    console.log("warmup Selected");
+    return page.waitForSelector('.ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled');
+}).then(function(){
+    
 })
