7a539755265cf32f1da88d67995d4bdfa87b05d1
mohd-aman <saifiamaan445@gmail.com>
2022-04-16 22:38:40 +0530

    hk login successful

--- module2/automation/hackerrank.js
+++ module2/automation/hackerrank.js
@@ -1,4 +1,6 @@
 const puppeteer = require("puppeteer");
+const mail = "gavic76183@carsik.com";
+const pass = "abcdef";
 
 let browserPromise = puppeteer.launch({ headless: false });
 let page;
@@ -17,5 +19,29 @@ browserPromise.then(function(browser){
     return waitPromise;
 }).then(function(){
     let clickPromse = page.click("ul.menu a");
-    return;
+    return clickPromse;
+}).then(function(){
+    let waitPromise = page.waitForSelector(".fl-module-content.fl-node-content .fl-button");
+    return waitPromise;
+}).then(function(){
+    let domClickPromse = page.evaluate(function(){
+        let btns = document.querySelectorAll(".fl-module-content.fl-node-content .fl-button");
+        btns[1].click();
+        return;
+    });
+    return domClickPromse;
+}).then(function(){
+    let waitPromise = page.waitForSelector("#input-1");
+    return waitPromise;
+}).then(function(){
+    let mailTypedPromise = page.type("#input-1",mail,{ delay: 100 });
+    return mailTypedPromise;
+}).then(function(){
+    let passTypedPromise = page.type("#input-2",pass,{delay:100});
+    return passTypedPromise;
+}).then(function(){
+    let clickPromse = page.click('button[data-analytics="LoginPassword"]');
+    return clickPromse;
+}).then(function(){
+    console.log("Login successful hogya hai ")
 })
