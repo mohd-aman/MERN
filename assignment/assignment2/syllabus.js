6204ccc6c2e878c15907a13e18239f4f55dbcad2
mohd-aman <saifiamaan445@gmail.com>
2022-04-26 21:25:00 +0530

    module 3 starting assignment 2 added

---
+++ assignment/assignment2/syllabus.js
@@ -0,0 +1,14 @@
+// -----------------Questions to Be Added-----------------------
+// pure functions -> FCC
+// mutations and side effects -> FCC
+// higher order functions -> FCC
+// map filter and reduce  -> FCC
+// polyfill -> Class
+// closure -> class
+// event loop and async architecture -> FCC
+// setTimeout and setInterval -> FCC
+// callbacks-> FCC
+
+//  references
+// * https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/#functional-programming
+// * https://github.com/getify/You-Dont-Know-JS/tree/1st-ed/async%20%26%20performance -> first 2 chapters
