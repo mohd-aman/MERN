0d3f46ff717a532f1a7a3b09ff4d1989356fa8e5
mohd-aman <saifiamaan445@gmail.com>
2022-05-07 20:57:01 +0530

    assignment3/test3 added

---
+++ assignment/test-3/test-3Syllabus.js
@@ -0,0 +1,25 @@
+// -----------------Questions to Be Added-----------------------
+
+// promises => then catch finally resolve reject create promise,all,race -> class
+// microtasks -> HW 
+// thunk -> HW 
+// shallow copy and deep copy  -> Class
+// null vs undefined vs undeclared -> class
+// error handling -> class
+// everything is object or primitive -> youtube  
+// References
+
+
+// * https://github.com/getify/You-Dont-Know-JS/tree/1st-ed/async%20%26%20performance -> first 3 chapters
+// * https://www.youtube.com/watch?v=fyGSyqEX2dw&t=1611s
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then
+//   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises
+//   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/catch
+//   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/race
+//   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/reject
+//   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/allSettled
+//   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
+//   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/finally
+// https://www.javascripttutorial.net/object/3-ways-to-copy-objects-in-javascript/ -> deep copy vs shallow
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Control_flow_and_error_handling -> error handling
+// https://betterprogramming.pub/javascript-interviews-whats-the-difference-between-a-variable-that-s-null-undefined-or-cb1c8f41e6c3 -> null ,undefined,undeclared 
