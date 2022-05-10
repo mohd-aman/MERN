beeb2e990c7a24acbf6b868523d8331f2bd1304b
mohd-aman <saifiamaan445@gmail.com>
2022-05-10 23:15:46 +0530

    ticket added with task only

--- module3/jira/script.js
+++ module3/jira/script.js
@@ -1,14 +1,42 @@
 let addBtn = document.querySelector(".add-btn");
-let modal = document.querySelector(".modal-cont");
+let modalCont = document.querySelector(".modal-cont");
+let taskAreaCont = document.querySelector(".textarea-cont");
+let mainCont = document.querySelector(".main-cont")
 let addModal = true;
 
 addBtn.addEventListener("click",function(){
     if(addModal){
         //show modal
-        modal.style.display = "flex";
+        modalCont.style.display = "flex";
     }else{
         //hide modal
-        modal.style.display = "none";
+        modalCont.style.display = "none";
     }
     addModal = !addModal;
 })
+
+modalCont.addEventListener("keydown",function(e){
+    let key = e.key;
+    if(key == 'Enter'){
+        createTicket(taskAreaCont.value);
+        taskAreaCont.value = "";
+        modalCont.style.display = "none";
+        addModal = !addModal;
+    }
+})
+
+
+
+function createTicket(task){
+    // <div class="ticket-cont">
+    //         <div class="ticket-color"></div>
+    //         <div class="ticket-id">#qzu03</div>
+    //         <div class="task-area">some task</div>
+    //     </div>
+    let ticketCont = document.createElement("div");
+    ticketCont.setAttribute('class','ticket-cont');
+    ticketCont.innerHTML = `<div class="ticket-color"></div>
+                            <div class="ticket-id">#qzu03</div>
+                            <div class="task-area">${task}</div>`
+    mainCont.appendChild(ticketCont);
+}
