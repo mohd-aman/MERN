c099da6d6c6fd00491893c3d528bb6f29f908146
mohd-aman <saifiamaan445@gmail.com>
2022-05-12 23:20:58 +0530

    chaning priority of ticket implemented

--- module3/jira/script.js
+++ module3/jira/script.js
@@ -6,7 +6,8 @@ let allPriorityColors = document.querySelectorAll(".priority-color");
 let removeBtn = document.querySelector(".remove-btn");
 let addModal = true;
 let removeFlag = false;
-let modalPriorityColor = 'black';
+let colors = ['lightpink','blue','green','black']
+let modalPriorityColor = colors[colors.length-1];
 
 addBtn.addEventListener("click",function(){
     if(addModal){
@@ -68,5 +69,22 @@ function createTicket(ticketColor,task){
     ticketCont.addEventListener("click",function(){
         if(removeFlag)
             ticketCont.remove();
+    })
+
+    //handle color
+    let ticketColorBand = ticketCont.querySelector(".ticket-color");
+    ticketColorBand.addEventListener("click",function(){
+        let currentTicketColor = ticketColorBand.classList[1];
+        let currentTicketColorIdx = -1;
+        for(let i=0;i<colors.length;i++){
+            if(currentTicketColor == colors[i]){
+                currentTicketColorIdx = i;
+                break;
+            }
+        }
+        let nextColorIdx = (currentTicketColorIdx+1)%colors.length;
+        let nextColor = colors[nextColorIdx];
+        ticketColorBand.classList.remove(currentTicketColor);
+        ticketColorBand.classList.add(nextColor);
     })
 }
