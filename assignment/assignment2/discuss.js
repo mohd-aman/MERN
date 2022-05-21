d538eb149296210ce116fa760d66aa96660c5370
mohd-aman <saifiamaan445@gmail.com>
2022-05-21 22:46:22 +0530

    setTimeout setInterval

--- assignment/assignment2/discuss.js
+++ assignment/assignment2/discuss.js
@@ -31,7 +31,7 @@
 // innerFn();
 
 //------------------>map 
-let arr = [1,2,3,4,5,6,7,8,9];
+// let arr = [1,2,3,4,5,6,7,8,9];
 // let newArr = arr.map(function(x){
 //     return 5*x;
 // });
@@ -49,8 +49,72 @@ let arr = [1,2,3,4,5,6,7,8,9];
 // console.log(filteredArr);
 // console.log(arr);
 
-let sum = arr.reduce(function(prev,ele){
-    return prev+ele;
-})
+// let sum = arr.reduce(function(prev,ele){
+//     return prev+ele;
+// })
 
-console.log(sum);
+// console.log(sum);
+
+// implement map
+
+// function myMap(arr,cb){
+//     let newArr = [];
+//     for(let i=0;i<arr.length;i++){
+//         let ele = arr[i];
+//         newArr.push(cb(ele));
+//     }
+//     return newArr;
+// }
+
+// let b = myMap(arr,function(x){return 5*x});
+// console.log(b);
+
+
+// implement filter
+
+// function myFilter(arr,cb){
+//     let newArr = [];
+//     for(let i=0;i<arr.length;i++){
+//         let ele = arr[i];
+//         if(cb(ele)){
+//             newArr.push(ele);
+//         }
+//     }
+//     return newArr;
+// }
+
+// let c = myFilter(arr,function(x){
+//     return x%2 == 1;
+// })
+// console.log(c);
+
+
+//-----------------> setTimeout
+
+// setTimeout(function(){
+//     console.log("Hello");
+// },5000)
+
+// console.log("World");
+
+
+
+//--------------> setInterval
+
+
+// let intervalId = setInterval(function(){
+//     console.log("Hello Repeated Many Times");
+// },3000);
+
+// setTimeout(function(){
+//     clearInterval(intervalId);
+// },10000);
+
+
+// setTimeout(function(){
+//     console.log("Will I ever run?");
+// },1000);
+
+// while(true){
+//     console.log("In the while loop");
+// }
