658b782baa621149b78749230e1b666e678af31e
mohd-aman <saifiamaan445@gmail.com>
2022-05-21 21:52:46 +0530

    map filter reduce

--- assignment/assignment2/discuss.js
+++ assignment/assignment2/discuss.js
@@ -20,12 +20,37 @@
 //Higher Order Function -> functions which accepts a function in parameter or returns a function;
 
 
-function func(){
-    return function(){
-        console.log("Inner function");
-    }
-}
+// function func(){
+//     return function(){
+//         console.log("Inner function");
+//     }
+// }
 
-let innerFn = func();
+// let innerFn = func();
 
-innerFn();
+// innerFn();
+
+//------------------>map 
+let arr = [1,2,3,4,5,6,7,8,9];
+// let newArr = arr.map(function(x){
+//     return 5*x;
+// });
+// console.log(newArr);
+
+
+// console.log(arr);
+
+//------------------>filter
+
+// let filteredArr = arr.filter(function(x){
+//     return x%2 == 0;
+// })
+
+// console.log(filteredArr);
+// console.log(arr);
+
+let sum = arr.reduce(function(prev,ele){
+    return prev+ele;
+})
+
+console.log(sum);
