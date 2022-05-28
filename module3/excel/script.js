6092572ca52b93db66f72c37ce025cd3f03be9e8
mohd-aman <saifiamaan445@gmail.com>
2022-05-28 21:36:37 +0530

    top left fixed

--- module3/excel/script.js
+++ module3/excel/script.js
@@ -30,3 +30,17 @@ function initCells(){
 }
 
 initCells();
+
+
+let topRow = document.querySelector(".top-row");
+let leftCol = document.querySelector(".left-col");
+let topLeftCell = document.querySelector(".top-left-cell");
+
+cellsContentDiv.addEventListener("scroll",function(e){
+    let scrollFromTop = e.target.scrollTop;
+    let scrollFromLeft = e.target.scrollLeft;
+    topRow.style.top = scrollFromTop+"px";
+    leftCol.style.left = scrollFromLeft+"px";
+    topLeftCell.style.top = scrollFromTop+"px";
+    topLeftCell.style.left = scrollFromLeft+"px";
+})
