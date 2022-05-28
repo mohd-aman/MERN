cffcf1a1aaa2ce85a7e22388372c0cbeffbc6b88
mohd-aman <saifiamaan445@gmail.com>
2022-05-28 22:22:51 +0530

    db init

---
+++ module3/excel/init.js
@@ -0,0 +1,53 @@
+let cellsContentDiv = document.querySelector(".cells-content");
+
+
+function initCells(){
+    let cellsContent = "<div class='top-left-cell'></div>";
+    //top-row
+    cellsContent+="<div class='top-row'>"
+    for(let i=0;i<26;i++){
+        cellsContent+=`<div class='top-row-cell'>${String.fromCharCode(65+i)}</div>`;
+    }
+    cellsContent+="</div>"
+
+    //left-col
+    cellsContent+="<div class='left-col'>"
+    for(let i=0;i<100;i++){
+        cellsContent+=`<div class='left-col-cell'>${i+1}</div>`
+    }
+    cellsContent+="</div>"
+    cellsContent+="<div class='cells'>"
+    for(let i=0;i<100;i++){
+        cellsContent+="<div class = 'row'>";
+        //column
+        for(let j=0;j<26;j++){
+            cellsContent+="<div class = 'cell' contentEditable></div>"
+        }
+        cellsContent+="</div>"
+    }
+    cellsContent+="</div>"
+    cellsContentDiv.innerHTML = cellsContent;
+}
+
+initCells();
+
+let db;
+
+function initDb(){
+    db = [];
+    for(let i=0;i<100;i++){
+        let row = [];
+        for(let j=0;j<26;j++){
+            let name = String.fromCharCode(65+j)+(i+1)+"";
+            let cellObject = {
+                name:name,
+                value:""
+            }
+            row.push(cellObject);
+        }
+        db.push(row);
+    }
+}
+initDb();
+
+console.log(db);
--- module3/excel/script.js
+++ module3/excel/script.js
@@ -1,37 +1,3 @@
-let cellsContentDiv = document.querySelector(".cells-content");
-
-
-function initCells(){
-    let cellsContent = "<div class='top-left-cell'></div>";
-    //top-row
-    cellsContent+="<div class='top-row'>"
-    for(let i=0;i<26;i++){
-        cellsContent+=`<div class='top-row-cell'>${String.fromCharCode(65+i)}</div>`;
-    }
-    cellsContent+="</div>"
-
-    //left-col
-    cellsContent+="<div class='left-col'>"
-    for(let i=0;i<100;i++){
-        cellsContent+=`<div class='left-col-cell'>${i+1}</div>`
-    }
-    cellsContent+="</div>"
-    cellsContent+="<div class='cells'>"
-    for(let i=0;i<100;i++){
-        cellsContent+="<div class = 'row'>";
-        //column
-        for(let j=0;j<26;j++){
-            cellsContent+="<div class = 'cell' contentEditable>CELL</div>"
-        }
-        cellsContent+="</div>"
-    }
-    cellsContent+="</div>"
-    cellsContentDiv.innerHTML = cellsContent;
-}
-
-initCells();
-
-
 let topRow = document.querySelector(".top-row");
 let leftCol = document.querySelector(".left-col");
 let topLeftCell = document.querySelector(".top-left-cell");
