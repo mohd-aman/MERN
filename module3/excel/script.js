a8f7d1c9dafcbe26eeeb4e0e9ee9c969a0288e80
mohd-aman <saifiamaan445@gmail.com>
2022-06-02 23:30:16 +0530

    formula to formula

--- module3/excel/script.js
+++ module3/excel/script.js
@@ -63,6 +63,9 @@ formulaInput.addEventListener("blur",function(e){
     if(formula){
         let {rowId,colId} = getRowIdColIdFromElement(lastSelectedCell);
         let cellObject = db[rowId][colId];
+        if(cellObject.formula){
+            removeFormula(cellObject);
+        }
         let computedValue = solveFormula(formula,cellObject); // will implement in next commit
         //update db
         cellObject.value = computedValue;
