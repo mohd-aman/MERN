5512f0b00406ad650d780c5b8e8500f3431a4ee1
mohd-aman <saifiamaan445@gmail.com>
2022-06-16 23:24:56 +0530

    react npx

---
+++ module4/react/todo-app/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Todo from './components/Todo.js';
+
+function App() {
+  return (
+   <Todo/>
+  );
+}
+
+export default App;
---
+++ module4/react/todo-app/src/components/Todo.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+
+class Todo extends Component{
+    render(){
+        return(
+            <div>This is Todo Component</div>
+        )
+    }
+}
+
+export default Todo;
