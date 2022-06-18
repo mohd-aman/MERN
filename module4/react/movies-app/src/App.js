0210ccdbb8796b8ff256566f0009575e1be1d5cb
mohd-aman <saifiamaan445@gmail.com>
2022-06-18 23:25:56 +0530

    Banner and boostrap link

--- module4/react/movies-app/src/App.js
+++ module4/react/movies-app/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Banner from './components/Banner';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <NavBar/>
-  );
+    <>
+      <NavBar/>
+      <Banner/>
+    </>
+    );
 }
 
 export default App;
