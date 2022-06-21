c7d5037e1c85a61315d179b5f0b3c9c5ca619df4
mohd-aman <saifiamaan445@gmail.com>
2022-06-21 21:35:25 +0530

    banner and navBar component styled

--- module4/react/movies-app/src/components/Banner.js
+++ module4/react/movies-app/src/components/Banner.js
@@ -4,13 +4,10 @@ import { Component } from "react";
 class Banner extends Component {
     render() {
         return (
-            <div className="card">
-                <img src="https://assets.reedpopcdn.com/digitalfoundry-2020-marvels-spider-man-miles-morales-tech-analysis-1604663057480.jpg/BROK/thumbnail/1600x900/quality/100/digitalfoundry-2020-marvels-spider-man-miles-morales-tech-analysis-1604663057480.jpg" className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
+            <div className="card banner-card">
+                <img src="https://assets.reedpopcdn.com/digitalfoundry-2020-marvels-spider-man-miles-morales-tech-analysis-1604663057480.jpg/BROK/thumbnail/1600x900/quality/100/digitalfoundry-2020-marvels-spider-man-miles-morales-tech-analysis-1604663057480.jpg" className="card-img-top banner-img" alt="..."/>
+                <h1 className="card-title banner-title">Spider-Man Miles Morale</h1>
+                <p className="card-text banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
         )
     }
--- module4/react/movies-app/src/components/NavBar.js
+++ module4/react/movies-app/src/components/NavBar.js
@@ -4,9 +4,9 @@ import { Component } from "react";
 class NavBar extends Component{
     render(){
         return(
-        <div style={{display:"flex",padding:'0.5rem'}}>
+        <div style={{display:"flex",padding:'0.5rem',color:"blue"}}>
             <h1>Movies App</h1>
-            <h2 style={{marginLeft:"2rem",marginTop:"2rem"}}>Favourites</h2>
+            <h2 style={{marginLeft:"2rem",marginTop:"0.5rem"}}>Favourites</h2>
         </div>
         )
     }
