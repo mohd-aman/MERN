c60c42eeda3602db9935ca354a6a08ddab2ad7d8
mohd-aman <saifiamaan445@gmail.com>
2022-06-21 23:40:22 +0530

    pagination state pArr added

--- module4/react/movies-app/src/components/MovieList.js
+++ module4/react/movies-app/src/components/MovieList.js
@@ -5,7 +5,8 @@ class MovieList extends Component {
     constructor(){
         super();
         this.state={
-            hover:""
+            hover:"",
+            pArr :[1]
         };
     }
 
@@ -32,9 +33,10 @@ class MovieList extends Component {
                 <nav aria-label="Page navigation example">
                     <ul className="pagination">
                         <li className="page-item"><a className="page-link" href="#">Previous</a></li>
-                        <li className="page-item"><a className="page-link" href="#">1</a></li>
-                        <li className="page-item"><a className="page-link" href="#">2</a></li>
-                        <li className="page-item"><a className="page-link" href="#">3</a></li>
+                        {this.state.pArr.map((ele)=>(
+                            <li className="page-item"><a className="page-link" href="#">{ele}</a></li>
+                        ))}
+                        
                         <li className="page-item"><a className="page-link" href="#">Next</a></li>
                     </ul>
                 </nav>
