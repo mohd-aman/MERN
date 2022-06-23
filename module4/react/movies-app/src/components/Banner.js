01dd161e1b0635034cb4912e616e2cdbc83b0539
mohd-aman <saifiamaan445@gmail.com>
2022-06-23 23:02:49 +0530

    movies from api call lifecycle method

--- module4/react/movies-app/src/components/Banner.js
+++ module4/react/movies-app/src/components/Banner.js
@@ -1,15 +1,31 @@
 import { Component } from "react";
 import {movies} from "../movieData"
+import axios from "axios"
 
 class Banner extends Component {
+    constructor(){
+        super();
+        this.state = {
+            movie:""
+        }
+    }
+    async componentDidMount(){
+        let res = await axios.get('https://api.themoviedb.org/3/trending/movie/day?api_key=81242a2aa2066e052c78ec9ac1700c59');
+        console.log("Banner");
+        console.log(res);
+        this.setState({
+            movie:res.data.results[0]
+        })
+
+    }
+
     render() {
-        let movie = movies.results[Math.floor(Math.random() * 10) ];
-        let backdrop_path = movie.backdrop_path;
+        let backdrop_path = this.state.movie.backdrop_path;
         return (
             <div className="card banner-card">
                 <img src={`https://image.tmdb.org/t/p/original${backdrop_path}`} className="card-img-top banner-img" alt="..."/>
-                <h1 className="card-title banner-title">{movie.title}</h1>
-                <p className="card-text banner-text">{movie.overview}</p>
+                <h1 className="card-title banner-title">{this.state.movie.title}</h1>
+                <p className="card-text banner-text">{this.state.movie.overview}</p>
             </div>
         )
     }
--- module4/react/movies-app/src/components/MovieList.js
+++ module4/react/movies-app/src/components/MovieList.js
@@ -1,24 +1,36 @@
 import { Component } from "react";
-import { movies } from "../movieData"
+// import { movies } from "../movieData"
+import axios from 'axios'
 
 class MovieList extends Component {
     constructor(){
         super();
         this.state={
             hover:"",
-            pArr :[1]
+            pArr :[1],
+            movies:[]
         };
     }
 
+    async componentDidMount(){
+        console.log("Component Did Mount");
+        const res = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=81242a2aa2066e052c78ec9ac1700c59&language=en-US&page=1')
+        console.log(res.data);
+        this.setState({
+            movies:[...res.data.results]
+        })
+    }
+
     render() {
-        let moviesArr = movies.results
+        console.log("rednered");
+        // let moviesArr = movies.results
         return (
             <>
                 <div>
                     <h3 className="text-center"><strong>Trending</strong></h3>
                 </div>
                 <div className="movies-list">
-                    {moviesArr.map((movieEle) => (
+                    {this.state.movies.map((movieEle) => (
                         <div className="card movie-card" onMouseEnter={()=>this.setState({hover:movieEle.id})} onMouseLeave={()=>this.setState({hover:""})} >
                             <img src={`https://image.tmdb.org/t/p/original${movieEle.backdrop_path}`} style={{ height: '40vh', width: '20vw' }} className="card-img-top movie-img" alt="..." />
                             <h5 className="card-title movie-title">{movieEle.title}</h5>
