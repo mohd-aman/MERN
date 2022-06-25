1d5a9a9a1ee49657fb49941ea69836203fc76d10
mohd-aman <saifiamaan445@gmail.com>
2022-06-25 23:05:05 +0530

    genre id updated

--- module4/react/movies-app/src/components/Fav.js
+++ module4/react/movies-app/src/components/Fav.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import {movies} from '../movieData'
+import { movies } from '../movieData'
 
 
 class Fav extends Component {
@@ -7,7 +7,7 @@ class Fav extends Component {
     render() {
         const moviesArr = movies.results
         console.log(moviesArr)
-        let genreIds = {28:"Action",12:"Adventure",16:"Animation",35:"Comedy",80:"Crime",99:"Documentary"}
+        let genreIds = { 28: "Action", 12: "Adventure", 16: "Animation", 35: "Comedy", 80: "Crime", 99: "Documentary", 18: "Drama", 10751: "Family", 14: "Fantasy", 36: "History", 27: "Horror", 10402: "Music", 9648: "Mystery", 10749: "Romance", 878: "Science Fiction", 10770: "TV Movie", 53: "Thriller", 10752: "War", 37: "Western"}
         return (
             <div className="container">
                 <div className="row">
@@ -38,9 +38,9 @@ class Fav extends Component {
                             </thead>
                             <tbody>
                                 {
-                                    moviesArr.map((movieEle)=>(
+                                    moviesArr.map((movieEle) => (
                                         <tr>
-                                            <th scope="row"><img  style={{width:"8rem",padding:"1rem"}} src={`https://image.tmdb.org/t/p/original${movieEle.backdrop_path}`}/>{  movieEle.title}</th>
+                                            <th scope="row"><img style={{ width: "8rem", padding: "1rem" }} src={`https://image.tmdb.org/t/p/original${movieEle.backdrop_path}`} />{movieEle.title}</th>
                                             <td>{genreIds[movieEle.genre_ids[0]]}</td>
                                             <td>{movieEle.popularity}</td>
                                             <td>{movieEle.vote_average}</td>
