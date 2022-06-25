1de8b4b8e20204fb009f6d715afa3ee196fe427c
mohd-aman <saifiamaan445@gmail.com>
2022-06-25 23:24:25 +0530

    categories based on movies

--- module4/react/movies-app/src/components/Fav.js
+++ module4/react/movies-app/src/components/Fav.js
@@ -8,16 +8,24 @@ class Fav extends Component {
         const moviesArr = movies.results
         console.log(moviesArr)
         let genreIds = { 28: "Action", 12: "Adventure", 16: "Animation", 35: "Comedy", 80: "Crime", 99: "Documentary", 18: "Drama", 10751: "Family", 14: "Fantasy", 36: "History", 27: "Horror", 10402: "Music", 9648: "Mystery", 10749: "Romance", 878: "Science Fiction", 10770: "TV Movie", 53: "Thriller", 10752: "War", 37: "Western"}
+        let tempArr = [];
+        tempArr.push("All genres")
+        moviesArr.map((movieObj)=>{
+            if(!tempArr.includes(genreIds[movieObj.genre_ids[0]])){
+                tempArr.push(genreIds[movieObj.genre_ids[0]])
+            }
+        })
+
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-3">
                         <ul className="list-group genre-selector">
-                            <li className="list-group-item">Favourites</li>
-                            <li className="list-group-item">Action</li>
-                            <li className="list-group-item">Action</li>
-                            <li className="list-group-item">Comedy</li>
-                            <li className="list-group-item">Adventure</li>
+                            {
+                                tempArr.map((genre)=>(
+                                    <li className="list-group-item">{genre}</li>
+                                ))
+                            }
                         </ul>
                     </div>
                     <div className="col-9 fav-table">
