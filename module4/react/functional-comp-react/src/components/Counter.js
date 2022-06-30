80c5d26af60919cd0dd96da2c7302bd016652cc4
mohd-aman <saifiamaan445@gmail.com>
2022-06-30 23:33:17 +0530

    input

--- module4/react/functional-comp-react/src/components/Counter.js
+++ module4/react/functional-comp-react/src/components/Counter.js
@@ -3,12 +3,15 @@ import { useState } from "react"
 
 function Counter(){
     const [count,setCount] = useState(0)
+    const [text,setText] = useState('')
 
     return(
         <div>
             <h1>This is a Counter {count}</h1>
             <button onClick={()=>setCount(count+1)}>+</button>
             <button onClick={()=>setCount(count-1)}>-</button>
+            <input value={text} onChange={(e)=>setText(e.target.value)}></input>
+            <h1>{text}</h1>
         </div>
     )
 }
