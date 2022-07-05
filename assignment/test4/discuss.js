e818a7895934eca732cdad534b4b02f32c8fa7b2
mohd-aman <saifiamaan445@gmail.com>
2022-07-05 22:06:43 +0530

    g3 , g9 and g4 completed

--- assignment/test4/discuss.js
+++ assignment/test4/discuss.js
@@ -13,3 +13,58 @@
 // console.log(a.next());
 // console.log(a.next());
 
+
+// function* reachInfinity(){
+//     for(let i=0;true;i++){
+//         yield i;
+//     }
+// }
+
+// let num = reachInfinity();
+// console.log(num.next());
+// console.log(num.next())
+// console.log("HI");
+
+// async function f1() {
+//     console.log(1);
+// }
+  
+// async function f1() {
+//   console.log(2);
+// }
+  
+// console.log(3);
+// f1();
+// console.log(1);
+// f2();
+  
+// async function f2() {
+//   console.log("Go!");
+// }
+
+
+// let userObj = {
+//     private:{
+//         dob:"12/3/4"
+//     },
+//     public:{
+//         name:"Anas"
+//     }
+// }
+
+// let handler = {
+//     get:function(target,prop){
+//         if(prop == "private"){
+//             return "Access not granted"
+//         }else{
+//             return target[prop];
+//         }
+//     }
+// }
+
+// let p = new Proxy(userObj,handler);
+
+// console.log(p.private);
+// console.log(p.public);
+
+// console.log(userObj.private);
