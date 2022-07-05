28f138f5aa9203941cff2e09f9dd5f35db8d5e3b
mohd-aman <saifiamaan445@gmail.com>
2022-07-05 21:41:38 +0530

    test 4 discussion q1 and q2 completed

---
+++ assignment/test4/discuss.js
@@ -0,0 +1,15 @@
+// function* abc(){
+//     console.log("Hello");
+//     console.log("HI");
+//     yield 1
+//     console.log("Hello again");
+//     console.log("Hi again")
+//     yield 2;
+//     console.log("bye");
+// }
+
+// let a = abc();
+// console.log(a.next());
+// console.log(a.next());
+// console.log(a.next());
+
