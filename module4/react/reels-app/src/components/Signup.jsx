1a8c263b60e8703f7689ea87b8b4dee9a6e3a26d
mohd-aman <saifiamaan445@gmail.com>
2022-07-16 23:31:36 +0530

    sign up funtionality

--- module4/react/reels-app/src/components/Signup.jsx
+++ module4/react/reels-app/src/components/Signup.jsx
@@ -1,8 +1,23 @@
-
-
+import {useState} from "react"
+import {auth} from "../firebase"
+import {createUserWithEmailAndPassword} from "firebase/auth"
 function Signup(){
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [name,setName] = useState("");
+    
+    async function processSignup(){
+        let userCred = await createUserWithEmailAndPassword(auth,email,password)
+        console.log(userCred.user);
+    }
+
     return(
-        <div>Sign up</div>
+        <>
+        <input type="email" onChange={(e)=>{setEmail(e.target.value)}}  value={email} placeholder="email"></input><br></br>
+        <input type="password" onChange={(e)=>{setPassword(e.target.value)}} value ={password} placeholder="password"></input><br></br>
+        <input type="text" onChange={(e)=>{setName(e.target.value)}} value={name} placeholder="Full Name"></input><br></br>
+        <button type="click" onClick={processSignup}>Sign up</button>
+        </>
     )
 }
 
