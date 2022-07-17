0448ff0573b180602b924353b64f61074e382ecc
mohd-aman <saifiamaan445@gmail.com>
2022-07-17 21:22:35 +0530

    sign up completed

--- module4/react/reels-app/src/components/Signup.jsx
+++ module4/react/reels-app/src/components/Signup.jsx
@@ -5,18 +5,37 @@ function Signup(){
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
     const [name,setName] = useState("");
+    const[loader,setLoader] = useState(false);
+    const[error,setError] = useState("");
+    const[user,setUser] = useState("");
     
     async function processSignup(){
-        let userCred = await createUserWithEmailAndPassword(auth,email,password)
-        console.log(userCred.user);
+        try{
+            setLoader(true);
+            let userCred = await createUserWithEmailAndPassword(auth,email,password)
+            // console.log(userCred.user);
+            setUser(userCred.user);
+        }catch(error){
+            setError(error.message);
+            setTimeout(()=>{
+                setError("")
+            },2000)
+        }
+        setLoader(false);
     }
 
     return(
         <>
-        <input type="email" onChange={(e)=>{setEmail(e.target.value)}}  value={email} placeholder="email"></input><br></br>
-        <input type="password" onChange={(e)=>{setPassword(e.target.value)}} value ={password} placeholder="password"></input><br></br>
-        <input type="text" onChange={(e)=>{setName(e.target.value)}} value={name} placeholder="Full Name"></input><br></br>
-        <button type="click" onClick={processSignup}>Sign up</button>
+        {error!=""?<h1>Error is {error}</h1>:
+            loader == true?<h1>...Loading</h1>:
+                user!=""?<h1>Sign up User is {user.uid}</h1>:
+            <>
+            <input type="email" onChange={(e)=>{setEmail(e.target.value)}}  value={email} placeholder="email"></input><br></br>
+            <input type="password" onChange={(e)=>{setPassword(e.target.value)}} value ={password} placeholder="password"></input><br></br>
+            <input type="text" onChange={(e)=>{setName(e.target.value)}} value={name} placeholder="Full Name"></input><br></br>
+            <button type="click" onClick={processSignup}>Sign up</button>
+            </>
+        }
         </>
     )
 }
