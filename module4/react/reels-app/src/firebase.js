fe2ca218d7552af2b289249bcba25e59bd0ad958
mohd-aman <saifiamaan445@gmail.com>
2022-07-17 22:13:31 +0530

    hide your API

--- module4/react/reels-app/src/firebase.js
+++ module4/react/reels-app/src/firebase.js
@@ -4,18 +4,11 @@ import {getAuth} from "firebase/auth"
 import {getFirestore} from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+import secret from "./secrets"
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAl1EE-KpHicnX0xC9nfootOZUaSvHrFb4",
-  authDomain: "class-demo-28b22.firebaseapp.com",
-  projectId: "class-demo-28b22",
-  storageBucket: "class-demo-28b22.appspot.com",
-  messagingSenderId: "254813831663",
-  appId: "1:254813831663:web:4825613e8a1fbed6a9f61a"
-};
+
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(secret);
 export let auth = getAuth(app);
 export const db = getFirestore(app);
