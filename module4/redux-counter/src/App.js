a322efd502d8d36addec32739018e70fdd159b5e
mohd-aman <saifiamaan445@gmail.com>
2022-07-26 22:00:04 +0530

    counter store reducer action

--- module4/redux-counter/src/App.js
+++ module4/redux-counter/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import {incrementCounter} from "./redux/action"
 
 function App() {
+  let state  = useSelector(function(state){
+    return state;
+  });
+  
+  let dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>{state}</p>
+      <button
+        onClick={()=>{//action dispatch
+            dispatch(incrementCounter());
+        }}
+      >+</button>
+    </>    
   );
 }
 
---
+++ module4/redux-counter/src/redux/action.js
@@ -0,0 +1,5 @@
+export const incrementCounter = ()=>{
+    return{
+        type:"INCREMENT",
+    }
+}
