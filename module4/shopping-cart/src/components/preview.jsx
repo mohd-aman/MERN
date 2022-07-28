26ca1c1f06cdad8036f77ce0739b5ba7a19d759c
mohd-aman <saifiamaan445@gmail.com>
2022-07-28 21:28:24 +0530

    preview added

--- module4/shopping-cart/src/components/preview.jsx
+++ module4/shopping-cart/src/components/preview.jsx
@@ -1,8 +1,20 @@
+import "./preview.css"
 
 let Preview = ()=>{
     return(
         <>
-        Preview
+            <div className="preview-container">
+                <div className="preview-img-container">
+                    <img src="https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/x/j/s/iphone-11-128-d-mwm02hn-a-apple-0-original-imafkg242ugz8hwc.jpeg?q=70" alt="" />
+                </div>
+                <div className="preview-listing">
+                    <h2>IPhone</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed rem libero quasi saepe voluptate et sapiente quam nostrum aut iure odit eos id, architecto tempore non eaque dolore, quibusdam animi.e
+                    </p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
         </>
     )
 }
