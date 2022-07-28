6a7bbc28dd6ef9e83d4077abb016cc2097ce216c
mohd-aman <saifiamaan445@gmail.com>
2022-07-28 22:29:25 +0530

    cart showing from state

--- module4/shopping-cart/src/components/cart.jsx
+++ module4/shopping-cart/src/components/cart.jsx
@@ -1,35 +1,44 @@
- 
+ import {useSelector} from "react-redux"
 
  let Cart = ()=>{
+  let state = useSelector((state)=>state);
+  let filteredArr = state.filter((el)=>el.qty>0)
+  let total = 0;
     return(
         <>
         <table class="table">
   <thead>
     <tr>
       <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Product Name</th>
+      <th scope="col">Price</th>
+      <th scope="col">Qty</th>
+      <th scope="col">Amount</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+  {
+    filteredArr.map((el,index)=>{
+      let amount = el.price*el.qty;
+      total+=amount
+      return(
+        <tr>
+      <th scope="row">{index+1}</th>
+      <td>{el.name}</td>
+      <td>{el.price}</td>
+      <td>{el.qty}</td>
+      <td>{amount}</td>
     </tr>
+      )
+    })
+  }
+    
     <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
+      <th ></th>
+      <td></td>
+      <td></td>
+      <th>Total</th>
+      <th>{total}</th>
     </tr>
   </tbody>
 </table>
--- module4/shopping-cart/src/redux/reducers.js
+++ module4/shopping-cart/src/redux/reducers.js
@@ -5,21 +5,21 @@ let initialState = [
         name:'IPhone',
         img:"https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/x/j/s/iphone-11-128-d-mwm02hn-a-apple-0-original-imafkg242ugz8hwc.jpeg?q=70",
         price:40000,
-        qty:0,
+        qty:1,
     },
     {
         id:1,
         name:"Kia car",
         img:"https://imgd.aeplcdn.com/0x0/n/cw/ec/115451/sonet-exterior-left-front-three-quarter.jpeg?isig=0",
         price:1000000,
-        qty:0,
+        qty:1,
     },
     {
         id:2,
         name:"PS 4",
         img:"https://www.reliancedigital.in/medias/Sony-CUH-2208B-Gaming-Console-491898064-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3w1NzQ2MTB8aW1hZ2UvanBlZ3xpbWFnZXMvaGNiL2gxYi85Mzc4MzMyNTQwOTU4LmpwZ3w4NTE2ZjkzYjQ2NWQwMDY0OTgzZjUxMGFiMmQxMWZlY2I0NGQ1ZjA4Y2RjN2ViOTQ1YzliYTIzNWM4Mjc4NWEw",
         price:30000,
-        qty:0
+        qty:1
     }
 ]
 
