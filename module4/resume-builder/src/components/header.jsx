9304b53403bc1fe56b44f2e4e3cd181a5946e44e
mohd-aman <saifiamaan445@gmail.com>
2022-07-30 22:48:00 +0530

    header and template

--- module4/resume-builder/src/components/header.jsx
+++ module4/resume-builder/src/components/header.jsx
@@ -1,8 +1,21 @@
-
+import "./header.css"
+import {Link} from "react-router-dom"
 
 const Header = ()=>{
     return(
-        <h1>Header</h1>
+        <div className="header">
+            <div className="header-logo">
+                <Link to="/"><img src="https://www.pepcoding.com/images/logo.png" alt="" /></Link>
+            </div>
+            <div className="header-links">
+                <ul>
+                    <li><Link to="/template">Resume Templates</Link></li>
+                    <li><Link to="/about">About Us</Link></li>
+                    <li><Link to="/register"><button class="btn">Register</button></Link></li>
+                    <li><Link to="/signin">Sign In</Link></li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
--- module4/resume-builder/src/components/template.jsx
+++ module4/resume-builder/src/components/template.jsx
@@ -1,8 +1,48 @@
-
+import "./template.css"
+import skin1 from "../static/images/skin1.svg"
+import skin2 from "../static/images/skin2.svg"
+import skin3 from "../static/images/skin3.svg"
+import skin4 from "../static/images/skin4.svg"
+import skin5 from "../static/images/skin5.svg"
+import skin6 from "../static/images/skin6.svg"
+import skin7 from "../static/images/skin7.svg"
+import skin8 from "../static/images/skin8.svg"
 
 const Template = ()=>{
     return(
-        <h1>Template</h1>
+        <div className="templates">
+            <div className="templates-intro">
+                <h1>Select a resume template</h1>
+                <p>you can edit and change it later!</p>
+            </div>  
+            <div className="templates-styles">
+                <div className="template">
+                    <img src={skin2}></img>
+                    <button className="template-btn">USE TEMPLATE</button>
+                </div>
+                <div className="template">
+                    <img src={skin3}></img>
+                </div>
+                {/* <div className="template">
+                    <img src={skin4}></img>
+                </div>
+                <div className="template">
+                    <img src={skin1}></img>
+                </div>
+                <div className="template">
+                    <img src={skin5}></img>
+                </div>
+                <div className="template">
+                    <img src={skin6}></img>
+                </div>
+                <div className="template">
+                    <img src={skin7}></img>
+                </div>
+                <div className="template">
+                    <img src={skin8}></img> */}
+                {/* </div> */}
+            </div>
+        </div>
     )
 }
 
