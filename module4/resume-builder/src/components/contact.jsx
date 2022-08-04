c63889731e69ab867c9e82e0acbdb9e29eb803d5
mohd-aman <saifiamaan445@gmail.com>
2022-08-04 22:49:49 +0530

    contact state

--- module4/resume-builder/src/components/contact.jsx
+++ module4/resume-builder/src/components/contact.jsx
@@ -1,9 +1,24 @@
 import Preview from "./preview"
 import "./contact.css"
+import { useSelector } from "react-redux"
+import { useState } from "react"
 
-function Contact(){
+function Contact(props){
+    const [contact,setContact] = useState({})
+    
+    const onChange = (event)=>{
+        let key = event.target.id;
+        let value = event.target.value;
+        setContact({...contact,[key]:value})
+    }
 
-
+    const getFieldData = (key)=>{
+        if(contact[key]){
+            return contact[key];
+        }
+        return "";
+    }
+    
     return(
         <div className="contact">
             <div className="contact-form">
@@ -13,41 +28,47 @@ function Contact(){
                 <div className="contact-form-details">
                     <div className="input-group">
                         <label htmlFor="">First Name</label>
-                        <input type="text" id="fname" value="Aman"/>
+                        <input type="text" id="fname" value={getFieldData("fname")} onChange={onChange}/>
                     </div>
                     <div className="input-group">
                         <label htmlFor="">Last Name</label>
-                        <input type="text" id="lname" value="Last Name"/>
+                        <input type="text" id="lname" value={getFieldData("lname")} onChange={onChange}/>
                     </div>
                     <div className="input-group full">
                         <label htmlFor="">Professional Summary</label>
-                        <input type="text" id="summary" value="summary professional"/>
+                        <input type="text" id="summary" value={getFieldData("summary")} onChange={onChange}/>
                     </div>
                     <div className="input-group">
                         <label htmlFor="">Email</label>
-                        <input type="text" id="email" value="abc@gmail.com"/>
+                        <input type="text" id="email" value={getFieldData("fname")} onChange={onChange}/>
                     </div><div className="input-group">
                         <label htmlFor="">Phone</label>
-                        <input type="text" id="phone" value="7849543"/>
+                        <input type="text" id="phone" value={getFieldData("fname")} onChange={onChange}/>
                     </div><div className="input-group">
                         <label htmlFor="">Professional</label>
-                        <input type="text" id="lname" value="Last Name"/>
+                        <input type="text" id="prof" value={getFieldData("prof")} onChange={onChange}/>
                     </div><div className="input-group">
                         <label htmlFor="">Street</label>
-                        <input type="text" id="street" value="Last Name"/>
+                        <input type="text" id="street" value={getFieldData("street")} onChange={onChange}/>
                     </div><div className="input-group">
                         <label htmlFor="">City</label>
-                        <input type="text" id="city" value="Last Name"/>
+                        <input type="text" id="city" value={getFieldData("city")} onChange={onChange}/>
                     </div><div className="input-group">
                         <label htmlFor="">State</label>
-                        <input type="text" id="state" value="Last Name"/>
+                        <input type="text" id="state" value={getFieldData("state")} onChange={onChange}/>
                     </div><div className="input-group">
                         <label htmlFor="">Country</label>
-                        <input type="text" id="country" value="Last Name"/>
+                        <input type="text" id="country" value={getFieldData("country")} onChange={onChange}/>
                     </div><div className="input-group">
                         <label htmlFor="">Pin Code</label>
-                        <input type="text" id="pin" value="Last Name"/>
+                        <input type="text" id="pin" value={getFieldData("pin")} onChange={onChange}/>
                     </div>
+                    <div className="next full">
+                        <div className="btn">Back</div>
+                   </div>
+                   <div className="back full">
+                        <div className="btn">Next</div>
+                   </div>
                 </div>
             </div>
             <div className="preview-form">
