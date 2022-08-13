8ea38cfe601e59e1e70f12f3405492a75b26a046
mohd-aman <saifiamaan445@gmail.com>
2022-08-13 22:26:41 +0530

    added middleware explanation

---
+++ module5/server3.js
@@ -0,0 +1,26 @@
+ const express = require("express")
+
+ const app = express();
+
+ app.use(express.json());
+
+app.post("/simple",function(req,res,next){
+    let data = req.body;
+    //Object.keys(data) => ["name",'age']
+    let length = Object.keys(data).length;
+    if(length == 0){
+        res.send("Kindly enter data in boody");
+    }else{
+        next();
+    }
+})
+
+app.post("/simple",function(req,res){
+    console.log("data",req.body);
+    res.send("Hello from Post 2");
+})
+
+
+ app.listen(3000,function(){
+    console.log("Server is started at 3000");
+ })
