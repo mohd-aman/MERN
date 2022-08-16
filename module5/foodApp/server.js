3e7a62dd02c0b6769330f342856cd15e8b2409bd
mohd-aman <saifiamaan445@gmail.com>
2022-08-16 21:50:38 +0530

    added upto schema, model and create document

--- module5/foodApp/server.js
+++ module5/foodApp/server.js
@@ -2,8 +2,29 @@ const  express = require("express")
 
 const app = express();
 
+app.use(express.json());
+
 const userModel = require("./userModel");
 
+//signup input:
+//name,
+//password
+//confirm password
+//address
+//email
+//phone
+//pic
+
+
+app.post("/signup",async function(req,res){
+    let data = req.body;
+    console.log(data);
+    let newUser =await userModel.create(data);
+    res.json({
+        message:"data recieved",
+        data:data
+    })
+})
 
 
 app.listen(3000,function(){
--- module5/foodApp/userModel.js
+++ module5/foodApp/userModel.js
@@ -1,12 +1,51 @@
 //tech knowledge
 //(scheme) -> set of features and rules a certain entity should follow
 const mongoose = require('mongoose')
+const {Schema} = mongoose
+const pass = require("./secrets");
 
-let dbLink =  `mongodb+srv://db_user:9fF4etNQdwcP83ME@cluster0.qj8is6o.mongodb.net/?retryWrites=true&w=majority`
-
+let dbLink = `mongodb+srv://dbUser:${pass}@cluster0.o6s3ey1.mongodb.net/?retryWrites=true&w=majority`
 mongoose.connect(dbLink).then(function(){
     console.log("connected");
 }).catch(function(err){
     console.log("error",err);
 })
 
+
+//how to create a schema -> only entries written will be added to your db no one else.
+
+let userSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    confirmPassword:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    phoneNumber:{
+        type:String,
+        minLength:10,
+        maxLength:10
+    },
+    pic:{
+        type:String,
+        default:"logo2.png"
+    },
+    address:{
+        type:String
+    }
+})
+
+const userModel = mongoose.model('FoodUserModel',userSchema);
+
+module.exports = userModel;
