d8d355862603d2da5e22da8ef7b7057cdd15bc5b
mohd-aman <saifiamaan445@gmail.com>
2022-08-23 23:31:46 +0530

    nodemailer integrated with foodApp

--- module5/foodApp/model/userModel.js
+++ module5/foodApp/model/userModel.js
@@ -2,7 +2,7 @@
 //(scheme) -> set of features and rules a certain entity should follow
 const mongoose = require('mongoose')
 const {Schema} = mongoose
-const pass = require("../secrets");
+const pass = require("../secret");
 
 let dbLink = `mongodb+srv://dbUser:${pass}@cluster0.o6s3ey1.mongodb.net/?retryWrites=true&w=majority`
 mongoose.connect(dbLink).then(function(){
