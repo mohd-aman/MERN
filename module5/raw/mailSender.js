872385e52350e5109a942c142bac7a2c5a0457e6
mohd-aman <saifiamaan445@gmail.com>
2022-08-23 22:55:52 +0530

    how to send email using nodemailer

---
+++ module5/raw/mailSender.js
@@ -0,0 +1,34 @@
+const nodemailer = require("nodemailer");
+const password = require("./secrets")
+
+async function mailSender() {
+
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "mohammad.aman@pepcoding.com", // generated ethereal user
+      pass: password, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "saifiamaan445@gmail.com, vasu786beri@gmail.com", // list of receivers
+    subject: "Hello ✔Testing for FJP6", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+mailSender().catch(console.error);
