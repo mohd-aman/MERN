dcd9afa1a40132f24d6a13fe40c989ff0d276967
mohd-aman <saifiamaan445@gmail.com>
2022-09-01 23:15:25 +0530

    module.export vs export

--- module5/raw/exportVsmoduleExport.js
+++ module5/raw/exportVsmoduleExport.js
@@ -22,22 +22,27 @@
 
 // console.log(module);
 
+// to use Es Modules instead of default commonjs module system in nodejs
+        //there are 2 ways 
+            //1-> change extension to .mjs from .js
+            //2->Or instead of changing the file extension, we can also add a property
+            //  "type":"module" at top level of package.json to use ES modules.
 
 //export
 //named export (multiple per module)
-export function sumGenerator(num1,num2){
-    return num1+num2;
-}
+// export function sumGenerator(num1,num2){
+//     return num1+num2;
+// }
 
-export function greeting(){
-    return "Hi Everyone"
-}
+// export function greeting(){
+//     return "Hi Everyone"
+// }
 
-export let myStr = "This is my string"
+// export let myStr = "This is my string"
 
 
-//default export
+// //default export
 
-export default function sumGenerator(num1,num2){
-    return num1+num2;
-}
+// export default function sumGenerator(num1,num2){
+//     return num1+num2;
+// }
